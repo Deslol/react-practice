f10d173551ebfb2ca602e44596081149b7ca7c65
Cyril Chan <kwunming0911@gmail.com>
2026-06-05 14:27:42 +0100

    Adding Testing page layout

--- src/components/TextAnimations/TextAnimation/TextAnimation.tsx
+++ src/components/TextAnimations/TextAnimation/TextAnimation.tsx
@@ -1,70 +1,92 @@
-import style from './TextAnimation.module.scss'
-import type {CSSProperties} from "react";
-
-
-// Should be replaced with props.rotationDegree
-function rng() {
-    return Math.random() * 60 - 30
-}
+import style from "./TextAnimation.module.scss";
+import type { CSSProperties } from "react";
 
 function textMoreThanOne(text: string) {
-    return text.includes(" ") ? (
-        <>
-            {text.split(" ")[0]}
-            <br/>
-            {text.split(" ")[1]}
-        </>
-    ) : (text)
+	return text.includes(" ") ? (
+		<>
+			{text.split(" ")[0]}
+			<br />
+			{text.split(" ")[1]}
+		</>
+	) : (
+		text
+	);
 }
 
 export default function TextAnimation({
-                                          // rotationDegree,
-                                          text,
-                                      }: {
-    text: string, // rotationDegree?: number
+	rotation = 0,
+	text,
+	onAnimationEnd,
+}: {
+	text: string;
+	rotation?: number;
+	onAnimationEnd?: () => void;
 }) {
-    return (
-        <div className={`${style.textAnimationContainer} flex justify-center align-middle`}>
-            <div className={style.animatedTextWrapper}
-                 style={({'--wrapper-rotation-deg': `${rng()}deg`} as CSSProperties)}>
-                <div className={style.textContainer}>
-                    <span className={style.mainText}>
-                        {textMoreThanOne(text)}
-                    </span>
-                    <span className={style.overlayText}>
-                         {textMoreThanOne(text)}
-                    </span>
-                    <span className={style.blurEffect}>
-                        {textMoreThanOne(text)}
-                    </span>
-                    <div className={style.explosiveLines}>
-                        <div
-                            className={style.lineWrapper}
-                            style={{'--angle': '45deg', '--line-height': '45px'} as CSSProperties}
-                        >
-                            <span className={style.line}/>
-                        </div>
-                        <div
-                            className={style.lineWrapper}
-                            style={{'--angle': '90deg', '--line-height': '125px'} as CSSProperties}
-                        >
-                            <span className={style.line}/>
-                        </div>
-                        <div
-                            className={style.lineWrapper}
-                            style={{'--angle': '245deg', '--line-height': '62.5px'} as CSSProperties}
-                        >
-                            <span className={style.line}/>
-                        </div>
-                        <div
-                            className={style.lineWrapper}
-                            style={{'--angle': '290deg', '--line-height': '187.5px'} as CSSProperties}
-                        >
-                            <span className={style.line}/>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
+	return (
+		<div
+			className={`${style.textAnimationContainer} flex justify-center align-middle`}
+		>
+			<div
+				className={style.animatedTextWrapper}
+				style={{ "--wrapper-rotation-deg": `${rotation}deg` } as CSSProperties}
+			>
+				<div className={style.textContainer}>
+					<span
+						className={style.mainText}
+						onAnimationEnd={(e) => {
+							if (e.target !== e.currentTarget) return;
+							onAnimationEnd?.();
+						}}
+					>
+						{textMoreThanOne(text)}
+					</span>
+					<span className={style.overlayText}>{textMoreThanOne(text)}</span>
+					<span className={style.blurEffect}>{textMoreThanOne(text)}</span>
+					<div className={style.explosiveLines}>
+						<div
+							className={style.lineWrapper}
+							style={
+								{ "--angle": "45deg", "--line-height": "45px" } as CSSProperties
+							}
+						>
+							<span className={style.line} />
+						</div>
+						<div
+							className={style.lineWrapper}
+							style={
+								{
+									"--angle": "90deg",
+									"--line-height": "125px",
+								} as CSSProperties
+							}
+						>
+							<span className={style.line} />
+						</div>
+						<div
+							className={style.lineWrapper}
+							style={
+								{
+									"--angle": "245deg",
+									"--line-height": "62.5px",
+								} as CSSProperties
+							}
+						>
+							<span className={style.line} />
+						</div>
+						<div
+							className={style.lineWrapper}
+							style={
+								{
+									"--angle": "290deg",
+									"--line-height": "187.5px",
+								} as CSSProperties
+							}
+						>
+							<span className={style.line} />
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
--- src/pages/TextAnimation/TextAnimationPage.tsx
+++ src/pages/TextAnimation/TextAnimationPage.tsx
@@ -2,18 +2,23 @@ import style from './TextAnimationPage.module.scss'
 import TextAnimation from "../../components/TextAnimations/TextAnimation/TextAnimation.tsx";
 import TextAnimationTwo from "../../components/TextAnimations/TextAnimationTwo/TextAnimationTwo.tsx";
 import TextAnimationThree from "../../components/TextAnimations/TextAnimationThree/TextAnimationThree.tsx";
+import CheeringTest from "../../components/TextAnimations/CheeringTest.tsx";
 
 export default function TextAnimationPage() {
 
     return (
-        <div className={`${style.textAnimationContainer} flex justify-center align-middle flex-col gap-10`}>
-            <TextAnimation text='Animation ONE'/>
+        // <div className={`${style.textAnimationContainer} flex justify-center align-middle flex-col gap-10`}>
+        //     <TextAnimation text='Animation ONE'/>
+        //
+        //     <TextAnimationTwo text="Animation TWO" />
+        //
+        //     <TextAnimationThree text="Animation Three" />
+        //
+        //     {/*<TextAnimation text='TWO'/>*/}
+        // </div>
 
-            <TextAnimationTwo text="Animation TWO" />
-
-            <TextAnimationThree text="Animation Three" />
-
-            {/*<TextAnimation text='TWO'/>*/}
-        </div>
+        <div className="w-full h-full">
+			<CheeringTest />
+		</div>
     )
 }
